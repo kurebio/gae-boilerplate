65d6ec348fb4c5fa490a7d493387444e9c06f4ba
KUMA <KUMA@KUMAC-NINE-11661.local>
2017-10-10 16:57:40 +0900

    Fixed settings.

--- dev.sh
+++ dev.sh
@@ -2,5 +2,4 @@
 
 # The private key is for authentication from dev server.
 # If you don't use authentication, delete the --appidentity_email_address and --appidentity_ priv_key_path arguments.
-dev_appserver.py app.yaml app-backend.yaml dispatch.yaml app-go/ --php_remote_debugging=yes
-#dev_appserver.py app.yaml app-backend.yaml app-go/ --php_remote_debugging=yes --appidentity_email_address kurebio-test-xxxxx@appspot.gserviceaccount.com --appidentity_private_key_path ./secret.pem
+dev_appserver.py app.yaml app-backend.yaml app-go/ dispatch.yaml --php_remote_debugging=yes --appidentity_email_address kurebio-test-xxxxx@appspot.gserviceaccount.com --appidentity_private_key_path ./secret.pem
